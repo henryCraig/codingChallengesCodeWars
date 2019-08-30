420181caad3cdf4db40b697209304fee4991289c
Henry Craig <h.craig52@yahoo.com>
2019-08-30 14:45:55 -0400

    the wave on a string challenge

---
+++ theWave/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theWave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wave(" gap ");
+            Console.ReadLine();
+        }
+
+        public static List<string> wave(string str)
+        {
+            List<string> returnList = new List<string>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetter(str[i]))
+                {
+                    returnList.Add(str.Substring(0, i).ToLower() + str[i].ToString().ToUpper() + str.Substring(i + 1, str.Length - i - 1));
+                }
+            }
+            return returnList;
+        }
+    }
+}
